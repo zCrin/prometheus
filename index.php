f2e77c3e9dfd280a0e2bc8e58e520957a625823c
zCrin <antoine.nga@icloud.com>
2017-02-23 22:02:18 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,74 @@
+<?php
+	session_set_cookie_params(0, '/', '.' . $_SERVER['SERVER_NAME']);
+	session_start();
+	if(isset($_COOKIE['device_allowed'])){
+		$json = json_decode(file_get_contents($_SERVER['DOCUMENT_ROOT'] . '/conf/allowed_devices.conf'), true);
+		if(in_array($_COOKIE['device_allowed'], $json['authorised_devices'])){
+			$_SESSION['allowed'] = true;
+		}
+	}
+	if(!isset($_SESSION['allowed'])){
+		$_SESSION['allowed'] = 'null';
+	} 
+	if($_SESSION['allowed'] != 'true'){
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php include_once('website/includes/head-content.include.php'); ?>
+		<?php include_once('website/includes/icons.include.php'); ?>
+		<?php include_once('website/includes/css.include.php'); ?>
+		<script src="website/js/jquery-1.11.3.min.js"></script>
+		<script src="website/js/stay_standalone.js"></script>
+		<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css"/>
+	</head>
+	<body>
+		<?php include_once('website/includes/header.include.php'); ?>
+		<div class='page' id='home_page'>
+			<div id='password_page'>
+				<div id='password_state'></div>
+				<div id='password_part'>
+					<div>
+						<button>1</button>							<button>2</button>
+						<button>3</button>
+					</div>
+					<div>
+						<button>4</button>							<button>5</button>
+						<button>6</button>
+					</div>
+					<div>
+						<button>7</button>							<button>8</button>
+						<button>9</button>
+					</div>
+					<div id='zero_button_password'>
+						<button>0</button>
+					</div>
+					<div id='delete_n'>Effacer</div>
+					<form action="website/exec/user_connect.php" id='validate' method="post">
+						<input type="hidden" id='password' name="password"/>
+					</form>
+				</div>
+			</div>
+			<div id='right_block'>
+				<div id="current_time"><?php echo date("G : i"); ?></div>
+				<?php         
+
+				?>
+				<div id="current_date"><?php show_date(); ?></div>
+			</div>
+		</div>
+	</body>
+	<script src="website/js/passcode_page.js"></script>
+</html>
+<?php
+}
+else{
+	header('location: /website');
+}
+function show_date(){
+	$mois = array('Janvier', 'F&eacute;vrier', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Ao&ucirc;t', 'Septembre', 'Octobre', 'Novembre', 'D&eacute;cembre'); 
+	$moisN = date("n") - 1;
+	$jours = array('Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche');
+	$jourN = date("N") - 1;
+	echo $jours[$jourN] . ' ' . date('j') . ' ' . $mois[$moisN];
+}
